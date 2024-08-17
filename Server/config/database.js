9b4c49a3ece7360000ac4c436d27426243fdc3fd
Sourav Bhattacherjee <sourav.1998.usb.sb@gmail.com>
2024-08-17 12:49:59 +0530

    Config added

--- Server/config/database.js
+++ Server/config/database.js
@@ -3,8 +3,17 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connectToMongo = () => {
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log("DB connected Successfully"))
-    .catch((error) => console.log("Error while Connectig with DB"));
+  const mongoose = require("mongoose");
+
+  require("dotenv").config();
+
+  exports.connectToMongo = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      poolSize: 10,
+    });
+  };
 };
